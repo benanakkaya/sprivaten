edd604c40296ada3556fb7410276b1bffc7ef78d
benanakkaya <ahmetbenanakkaya@gmail.com>
2023-08-04 17:04:25 +0300

    fix

--- src/components/ServiceVideo/ServiceVideo.jsx
+++ src/components/ServiceVideo/ServiceVideo.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {FaPlay} from "react-icons/fa"
+import {GiPlayButton} from "react-icons/gi"
 
 const ServiceVideo = () => {
   return (
@@ -16,7 +16,7 @@ const ServiceVideo = () => {
             <div className="service-video-content">
                 <div className="service-video-content-cover">
                     <span className="service-video-content-cover-button">
-                    <FaPlay />
+                    <GiPlayButton />
                     </span>
                 </div>
             </div>
